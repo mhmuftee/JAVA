c94bf2c2391df18b89b65bfc1c5a42daffaeaf84
Muftee <1@gmail.com>
2015-08-25 10:16:50 +0600

    Uploading For_each.java

---
+++ For_each.java
@@ -0,0 +1,8 @@
+class For_each{
+  public static void main(String[] args){
+    String[] names = {"one","two","three","four","five"};
+
+    for(String name:names)
+      System.out.println(name);
+  }
+}
