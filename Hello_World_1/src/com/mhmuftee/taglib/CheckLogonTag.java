86a6a3a62a78bd7ec14ac3220728eda0aa2ac602
mahfuzul.muftee <mahfuzul.muftee@bjitgroup.com>
2016-05-20 19:34:47 +0600

    Hello_World_1_Second_Commit

---
+++ Hello_World_1/src/com/mhmuftee/taglib/CheckLogonTag.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.mhmuftee.taglib;
+
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * @author Mahfuzul Haque Muftee May 20, 2016
+ * 
+ */
+public class CheckLogonTag extends TagSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	public CheckLogonTag() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
