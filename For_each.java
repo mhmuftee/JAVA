a38caaa6a7506516dbdad377cd34c5c0fee445e1
Md. Mahfuzul Haque Muftee <mhmuftee@gmail.com>
2016-06-08 12:26:00 +0600

    Delete For_each.java

--- For_each.java
+++
@@ -1,8 +0,0 @@
-class For_each{
-  public static void main(String[] args){
-    String[] names = {"one","two","three","four","five"};
-
-    for(String name:names)
-      System.out.println(name);
-  }
-}
